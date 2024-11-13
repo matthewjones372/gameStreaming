80bdc30813c361379d1bad6d0a64220abe75a7e1
matt <matthewjones372@gmail.com>
2024-11-13 15:16:20 +0000

    Spotted an improvement 4+ years later...

--- build.sbt
+++ build.sbt
@@ -2,18 +2,18 @@ name := "GamingStream"
 
 version := "0.1"
 
-scalaVersion := "2.13.3"
+scalaVersion := "2.13.12"
 
-lazy val refinedVersion = "0.9.15"
+lazy val refinedVersion = "0.11.1"
 lazy val catsVersion = "2.0.0"
-lazy val scalaLoggingVersion = "3.9.2"
-lazy val logbackVersion = "1.2.3"
+lazy val scalaLoggingVersion = "3.9.5"
+lazy val logbackVersion = "1.4.12"
 
 //Test dependencies
-lazy val scalaTestVersion = "3.2.1"
-lazy val scalaCheckVersion = "1.14.3"
-lazy val scalaCheckMagnoliaVersion = "0.4.0"
-lazy val scalaCheckTestPlus = "3.1.0.0"
+lazy val scalaTestVersion = "3.2.19"
+lazy val scalaCheckVersion = "1.18.0"
+lazy val scalaCheckMagnoliaVersion = "0.6.0"
+lazy val scalaCheckTestPlus = "3.2.2.0"
 
 libraryDependencies ++= Seq(
   "eu.timepit" %% "refined" % refinedVersion,
@@ -24,5 +24,3 @@ libraryDependencies ++= Seq(
   "com.github.chocpanda" %% "scalacheck-magnolia" % scalaCheckMagnoliaVersion % Test,
   "org.scalatestplus" %% "scalacheck-1-14" % scalaCheckTestPlus % Test
 )
-
-scalafmtOnCompile := true
--- src/main/scala/GameStreaming/BasketballEventParser.scala
+++ src/main/scala/GameStreaming/BasketballEventParser.scala
@@ -1,65 +1,44 @@
 package GameStreaming
 
 import GameStreaming.BasketballEvent.TeamScored
-import GameStreaming.BasketballPoint.{OnePointer, ThreePointer, TwoPointer}
-import GameStreaming.BasketballTeam.{Team1, Team2}
+import GameStreaming.BasketballPoint.{ OnePointer, ThreePointer, TwoPointer }
+import GameStreaming.BasketballTeam.{ Team1, Team2 }
 import GameStreaming.EventFormat.EventFormatSpecification
 import GameStreaming.EventFormat.EventFormatSpecification.Offset
 import eu.timepit.refined.types.numeric.NonNegInt
-
-import scala.util.{Failure, Success, Try}
+import scala.util.{ Failure, Success, Try }
 
 class BasketballEventParser(eventFormat: EventFormatSpecification) extends EventParser {
   import BasketballEventParser._
 
   override def parseEvent(event: String): Either[BasketballEventError, TeamScored] =
-    for {
-      bitVector  <- convertToBinaryList(event)
-      score      <- decodeScore(decodeSegment(bitVector, eventFormat.MatchScore))
-      teamScored <- decodeScoringTeam(decodeSegment(bitVector, eventFormat.ScoringTeam))
-      team1score <- decodeTeamScore(decodeSegment(bitVector, eventFormat.Team1Score))
-      team2Score <- decodeTeamScore(decodeSegment(bitVector, eventFormat.Team2Score))
-      matchTime  <- decodeGameTime(decodeSegment(bitVector, eventFormat.MatchTime))
-    } yield TeamScored(score, teamScored, GameState(team1score, team2Score, matchTime))
-
-  private def convertToBinaryList(event: String): Either[InvalidEventString, List[Char]] =
-    Try {
-      Integer
-        .decode(event)
-        .toInt
-        .toBinaryString
-        .reverse
-        .padTo(eventFormat.BitLength, 0)
-        .mkString
-        .toList
-    } match {
-      // This could be done simply with cats leftMap
-      case Success(bits) => Right(bits)
-      case Failure(_)    => Left(InvalidEventString(s"Could not parse input event: $event"))
+    convertToInt(event).flatMap { bitVector =>
+      for {
+        score      <- decodeScore(getDecodedSegment(bitVector, eventFormat.MatchScore))
+        teamScored <- decodeScoringTeam(getDecodedSegment(bitVector, eventFormat.ScoringTeam))
+        team1score <- decodeTeamScore(getDecodedSegment(bitVector, eventFormat.Team1Score))
+        team2Score <- decodeTeamScore(getDecodedSegment(bitVector, eventFormat.Team2Score))
+        matchTime  <- decodeGameTime(getDecodedSegment(bitVector, eventFormat.MatchTime))
+      } yield TeamScored(score, teamScored, GameState(team1score, team2Score, matchTime))
     }
-}
-object BasketballEventParser {
-  final case class InvalidEventString(msg: String) extends BasketballEventError
-  final case class InvalidScore(msg: String) extends BasketballEventError
-  final case class TeamScoreParseError(msg: String) extends BasketballEventError
-  final case class GameTimeParseError(msg: String, error: String) extends BasketballEventError
 
-  private def decodeSegment(eventVector: Seq[Char], offset: Offset): Int = {
-    val segmentString = eventVector.slice(offset.from, offset.to).reverse.mkString
-    Integer.valueOf(segmentString, 2)
+  private def convertToInt(event: String): Either[InvalidEventString, Int] = {
+    Try(Integer.decode(event).toInt) match {
+      case Success(value) => Right(value)
+      case Failure(_)     => Left(InvalidEventString(s"Could not parse input event: $event"))
+    }
+  }
+
+  private def getDecodedSegment(bitVector: Int, offset: Offset): Int = {
+    val mask = (1 << (offset.to - offset.from)) - 1
+    (bitVector >> offset.from) & mask
   }
 
   private def decodeTeamScore(teamScore: Int): Either[TeamScoreParseError, NonNegInt] =
-    NonNegInt.from(teamScore) match {
-      case Left(_)      => Left(TeamScoreParseError("Team Scores must be Non-Negative"))
-      case Right(value) => Right(value)
-    }
+    NonNegInt.from(teamScore).left.map(_ => TeamScoreParseError("Team Scores must be Non-Negative"))
 
   private def decodeGameTime(gameTime: Int): Either[GameTimeParseError, NonNegInt] =
-    NonNegInt.from(gameTime) match {
-      case Left(error)  => Left(GameTimeParseError("Game Time must be Non-Negative", error))
-      case Right(value) => Right(value)
-    }
+    NonNegInt.from(gameTime).left.map(error => GameTimeParseError("Game Time must be Non-Negative", error))
 
   private def decodeScore(pointScore: Int): Either[InvalidScore, BasketballPoint] =
     pointScore match {
@@ -75,4 +54,11 @@ object BasketballEventParser {
       case 1 => Right(Team2)
       case _ => Left(TeamScoreParseError(s"Scoring team: $scoringBit is not possible"))
     }
+}
+
+object BasketballEventParser {
+  final case class InvalidEventString(msg: String)                          extends BasketballEventError
+  final case class InvalidScore(msg: String)                                extends BasketballEventError
+  private final case class TeamScoreParseError(msg: String)                 extends BasketballEventError
+  private final case class GameTimeParseError(msg: String, error: String)() extends BasketballEventError
 }
